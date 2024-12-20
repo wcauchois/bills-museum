af474a115a8419ed72ea61e391c0bb752cd32ec2
Bill Cauchois <wcauchois@gmail.com>
2024-12-20 18:33:41 -0500

    texture testing kinda working

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { worker } from "./worker/workerClient"
 import * as THREE from "three"
 import { InputController } from "./game/InputController"
 import { FirstPersonCamera } from "./game/FirstPersonCamera"
+import { Sky } from "three/addons/objects/Sky.js"
 
 // console.log(await worker.getRelevantQuotes("death"))
 
@@ -12,19 +13,71 @@ const scene = new THREE.Scene()
 ;(window as any).globalScene = scene
 
 const light = new THREE.DirectionalLight(0xd5deff)
-light.position.x = 4
-light.position.y = 5
-light.position.z = 1
+light.position.x = -50
+light.position.y = 50
+light.position.z = 25
 scene.add(light)
 // const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1)
 // scene.add(light)
 
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshLambertMaterial({
+const material = new THREE.MeshPhongMaterial({
+	shininess: 100,
 	color: 0x00ff00,
 })
 const cube = new THREE.Mesh(geometry, material)
+cube.position.x = 0
+cube.position.y = 0
+cube.position.z = 0
 scene.add(cube)
+
+const planeGeometry = new THREE.PlaneGeometry(20, 5)
+const planeMaterial = new THREE.MeshLambertMaterial({
+	color: 0xffffff,
+	side: THREE.DoubleSide,
+})
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+planeMesh.position.x = 5
+planeMesh.rotation.y = THREE.MathUtils.degToRad(90)
+scene.add(planeMesh)
+
+const drawCanvas = document.createElement("canvas")
+drawCanvas.width = 512
+drawCanvas.height = 512
+const drawContext = drawCanvas.getContext("2d")!
+
+drawContext.fillStyle = "red"
+
+drawContext.font = "50px serif"
+drawContext.fillText("Hello, World!", 50, 90)
+
+const tex = new THREE.CanvasTexture(drawCanvas)
+
+const plane2Geometry = new THREE.PlaneGeometry(20, 5)
+const plane2Material = new THREE.MeshBasicMaterial({
+	color: 0xff0000,
+	side: THREE.DoubleSide,
+	map: tex,
+})
+
+const plane2Mesh = new THREE.Mesh(plane2Geometry, plane2Material)
+plane2Mesh.position.x = -5
+plane2Mesh.rotation.y = THREE.MathUtils.degToRad(180)
+scene.add(plane2Mesh)
+
+const sky = new Sky()
+sky.scale.setScalar(450000)
+
+const phi = THREE.MathUtils.degToRad(90)
+const theta = THREE.MathUtils.degToRad(180)
+const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta)
+
+sky.material.uniforms.sunPosition.value = sunPosition
+
+scene.add(sky)
 
 const camera = new THREE.PerspectiveCamera(
 	75,

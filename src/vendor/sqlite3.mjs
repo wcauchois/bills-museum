9dcb98befecadde5dc78ef2ebdb966d6fd5f91a0
Bill Cauchois <wcauchois@gmail.com>
2024-12-22 19:28:48 -0500

    more opfs

--- src/vendor/sqlite3.mjs
+++ src/vendor/sqlite3.mjs
@@ -16982,7 +16982,7 @@ const installOpfsVfs = function callee(options){
   return thePromise;
 }/*installOpfsVfs()*/;
 installOpfsVfs.defaultProxyUri =
-  "sqlite3-opfs-async-proxy.js";
+  "/sqlite3-opfs-async-proxy.js";
 globalThis.sqlite3ApiBootstrap.initializersAsync.push(async (sqlite3)=>{
   try{
     let proxyJs = installOpfsVfs.defaultProxyUri;

d7b0086c642e30f9ff89cd9bf91dfa71691324ce
Bill Cauchois <wcauchois@gmail.com>
2024-12-26 21:48:29 -0500

    woops file

---
+++ scripts/billQuotes.ts
@@ -0,0 +1,63 @@
+export const billQuotes: Array<{ text: string; author: string }> = [
+	{
+		text: `The world is full of power and energy and a person can go far by just skimming off a tiny bit of it.`,
+		author: "Neal Stephenson",
+	},
+	{
+		text: `If the scanner sees only darkly, the way I do, then I'm cursed and cursed again.`,
+		author: "Phillip K. Dick",
+	},
+	{
+		// the disposessed
+		text: `Even from the brother there is no comfort in the bad hour, in the dark at the foot of the wall.`,
+		author: "Ursula K. Le Guin",
+	},
+	{
+		text: `I had learned in the intervening time the folly of limiting desire to the possible.`,
+		author: "Gene Wolfe",
+	},
+	{
+		text: `An occult agedness. A transparent darkness. A broken and mysterious grove. A tragedy, a glory, a decay.`,
+		author: "Mervyn Peake",
+	},
+	// {
+	// 	text: "A gush of dark love.",
+	// 	author: "Mervyn Peake",
+	// },
+	{
+		text: `The laudanum built for him within his skull a tallow coloured world of ghastly beauty.`,
+		author: "Mervyn Peake",
+	},
+	{
+		text: `She could feel the blood flowing within her and she felt that she must die or break forth into leaves and flowers.`,
+		author: "Mervyn Peake",
+	},
+	{
+		text: `He felt like he had left the real world and entered another, like he was meeting a spirit in the forest`,
+		author: "Paul Murray",
+	},
+	{
+		text: `What is the world but a shadowy army of Elaine’s potential lovers?`,
+		author: "Paul Murray",
+	},
+	{
+		text: `In the sepia monochrome of a rainy day in that lost village they’d grown old instantly.`,
+		author: "Cormac McCarthy",
+	},
+	{
+		text: `Perhaps there is another dimension perpendicular to it, invisible to those laws of physics, describing the same things with different rules, and those rules are written in our hearts, in a deep place where we cannot go and read them except in our dreams.`,
+		author: "Neal Stephenson",
+	},
+	{
+		text: `I am sick of roses, and I am horny for revenge.`,
+		author: "Tamsyn Muir",
+	},
+	{
+		text: `I want wonderful and fascinating and marvelous things to happen to me and I don’t want to do anything to make them happen. Nothing at all.`,
+		author: "Samuel R. Delany",
+	},
+	{
+		text: "Then a smile poured into her face, mixing with the sleep like milk poured into water, while her eyes cleared like jade.",
+		author: "Samuel R. Delany",
+	},
+]
